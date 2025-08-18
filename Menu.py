fa2e2c1c50a93e448bbdd725cdefda95209ffc2d
PedroGuiOrtolaniRocha <pedrogui.rocha@gmail.com>
2025-08-18 00:38:12 -0300

    fixing menu and api improve

--- Menu.py
+++ Menu.py
@@ -1,38 +1,6 @@
-import DataHandler
 import os
 import asyncio
-from model_morador import Morador
-
-dt_handler = DataHandler.DataHandler("PREDIO.db")
-
-async def cadastro_morador():
-    nome = input("Nome do morador: ")
-    bloco = input("Bloco do morador: ")
-    ap = int(input("Número do apartamento: "))
-    senha = input("Senha do morador: ")
-    morador = Morador(nome, bloco, ap, senha)
-    await dt_handler.insert_morador(morador)
-    print("Morador cadastrado com sucesso!")
-
-async def listar_moradores():
-    moradores = await dt_handler.get_moradores()
-    if not moradores:
-        print("Nenhum morador cadastrado.")
-    else:
-        print("Moradores cadastrados:")
-        for morador in moradores:
-            print(f"ID: {morador.id}, \nNome: {morador.nome}, \nBloco: {morador.bloco}, \nAp: {morador.ap}, \nEm casa: {morador.em_casa}, \nData de entrada: {morador.data_entrada}, \nData de saída: {morador.data_saida}, \nData de cadastro: {morador.data_cadastro}\n\n")
-
-async def get_morador():
-
-    id = input("ID do morador: ")
-    morador = await dt_handler.get_morador(id)
-
-    if morador is None:
-        print("Morador não encontrado.")
-
-    else:
-        print(f"ID: {morador.id}, \nNome: {morador.nome}, \nBloco: {morador.bloco}, \nAp: {morador.ap}, \nEm casa: {morador.em_casa}, \nData de entrada: {morador.data_entrada}, \nData de saída: {morador.data_saida}, \nData de cadastro: {morador.data_cadastro}\n\n")
+import menu_morador
 
 def menu():
     while True:
@@ -40,19 +8,28 @@ def menu():
         print("1. Cadastrar morador")
         print("2. Listar moradores")
         print("3. Buscar morador por ID")
-        print("4. Sair")
+        print("4. Deletar morador")
+        print("5. Registrar entrada/saída")
+        print("6. Registrar entrada por senha")
+        print("7. Sair")
         opcao = input("Escolha uma opção: ")
         
         os.system('cls' if os.name == 'nt' else 'clear')
 
         if opcao == "1":
-            asyncio.run(cadastro_morador())
+            asyncio.run(menu_morador.cadastro_morador())
         elif opcao == "2":
-            asyncio.run(listar_moradores())
+            asyncio.run(menu_morador.listar_moradores())
         elif opcao == "3":
-            asyncio.run(get_morador())
+            asyncio.run(menu_morador.get_morador())
         elif opcao == "4":
-            dt_handler.close_connection()
+            asyncio.run(menu_morador.delete_morador())
+        elif opcao == "5":
+            asyncio.run(menu_morador.registro_entrada_saida())
+        elif opcao == "6":
+            asyncio.run(menu_morador.registro_entrada_senha())    
+        elif opcao == "7":
+            menu_morador.dt_handler.close_connection()
             print("Saindo...")
             break
         else:
